4c8818470b73e3268e513ed73a7fc2b5a0532a4c
ninkovski <75104211+ninkovski@users.noreply.github.com>
2026-02-19 12:38:39 -0500

    Update ResumeController.java

--- src/main/java/com/backend/java/microservice/controller/ResumeController.java
+++ src/main/java/com/backend/java/microservice/controller/ResumeController.java
@@ -11,7 +11,6 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 @SecurityRequirement(name = "ApiKeyAuth")
 public class ResumeController {
 
